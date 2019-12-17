bd0c6467afdcc6331ba522bf2380162d3e076f82
tsuzakitakehiro <tsuzakitakehiro@team-lab.com>
2019-12-17 12:40:04 +0900

    初期プロジェクト作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={() => <h1>Root Page</h1>} />
+      <Route path="/watch/:id" component={() => <h1>Watch Page</h1>} />
+      <Route component={() => <h1>404</h1>} />
+    </Switch>
+  </BrowserRouter>
+)
 
 export default App;
